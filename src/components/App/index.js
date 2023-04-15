c54bee7fc9c2978247e34fae569f192cfbe4f961
Belinda Shan <belindashan126@hotmail.com>
2023-04-15 12:38:42 +1000

    created a search form in Search component, added state

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from "react";
+import Search from "../HomePage/Search";
 
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <div>
+      <h1>Hi Cuties!</h1>
+      <Search />
+    </div>
+  );
 }
--- src/components/HomePage/Search.js
+++ src/components/HomePage/Search.js
@@ -1,8 +1,50 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+
+const Animals = ["dog", "cat", "bird"];
+const Breeds = [];
 
 export default function Search() {
-  return (
-    <div>Search</div>
-  )
-}
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
 
+  return (
+    <div>
+      <form>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => setAnimal(e.target.value)}
+            onBlur={(e) => setAnimal(e.target.value)}
+          >
+            <option />
+            {Animals.map((animal) => (
+              <option value={animal} key={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+            onBlur={(e) => setBreed(e.target.value)}
+          >
+            <option />
+            {Breeds.map((breed) => (
+              <option value={breed} key={breed}>
+                {breed}
+              </option>
+            ))}
+          </select>
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
