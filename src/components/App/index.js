3d846f80b4c033c01b460ccb81b408d59c5896e9
Belinda Shan <belindashan126@hotmail.com>
2023-04-16 20:21:01 +1000

    added CSS to component App and HomePage using Bootstrap

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,11 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "../HomePage/HomePage";
 import Detail from "../DetailPage";
 import FavoritesPage from "../FavoritesPage";
 import NotFoundPage from "../NotFoundPage";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Navbar } from "react-bootstrap";
 
 export const FavoritesContext = React.createContext({
   favorites: [],
@@ -15,9 +17,13 @@ export default function App() {
   return (
     <FavoritesContext.Provider value={{ favorites, setFavorites }}>
       <BrowserRouter>
-        <header>
-          <Link to="/">Hi Cuties!</Link>
-        </header>
+        <Navbar bg="light" variant="light">
+          <Container>
+            <Navbar.Brand href="/">
+              <i className="bi bi-flower1"></i> Hi Cuties!
+            </Navbar.Brand>
+          </Container>
+        </Navbar>
         <Routes>
           <Route path="*" element={<NotFoundPage />} />
           <Route path="/" element={<HomePage />} />
--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { FavoritesContext } from "../App";
 import PetList from "./PetList";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col, Image, Nav, Button } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 const ANIMALS = ["cat", "dog", "bird", "rabbit", "reptile"];
 
@@ -37,54 +40,93 @@ export default function HomePage() {
   };
 
   return (
-    <div>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          handleSubmit();
-        }}
-      >
-        <label htmlFor="animal">
-          Animal
-          <select
-            id="animal"
-            value={animal}
-            onChange={(e) => {
-              setAnimal(e.target.value);
-            }}
-            onBlur={(e) => {
-              setAnimal(e.target.value);
+    <Container>
+      <Row className="px-4 my-5">
+        <Col sm={8}>
+          {" "}
+          <p className="fs-4">
+            Welcome to our pet photo showcase website, where you can browse
+            through adorable pictures of all kinds of pets! From fluffy cats to
+            playful dogs, we've got it all. Each photo is accompanied by basic
+            information about the pet, such as their name, breed and a short
+            description of their personality. We're dedicated to showcasing the
+            beauty and uniqueness of each and every pet, and we hope that our
+            website will bring a smile to your face and brighten up your day. So
+            come on in, take a look around, and maybe even find your new furry
+            friend!
+          </p>
+        </Col>
+        <Col sm={4}>
+          <Image src="https://i.chzbgr.com/full/8194376448/h549EF8E8/do-a-birdie-roll" />
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={12} md={8}>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleSubmit();
             }}
           >
-            <option />
-            {ANIMALS.map((animal) => {
-              return (
-                <option value={animal} key={animal}>
-                  {animal}
-                </option>
-              );
-            })}
-          </select>
-        </label>
-        <label htmlFor="breed">
-          Breed
-          <select
-            id="breed"
-            value={breed}
-            onChange={(e) => setBreed(e.target.value)}
-            onBlur={(e) => setBreed(e.target.value)}
-          >
-            <option />
-            {breedOptions.map((breed) => (
-              <option value={breed} key={breed}>
-                {breed}
-              </option>
-            ))}
-          </select>
-        </label>
-        <button>Submit</button>
-      </form>
-      <Link to={"/favorite"}>Favorites</Link>
+            <label htmlFor="animal">
+              Animal
+              <select
+                id="animal"
+                value={animal}
+                onChange={(e) => {
+                  setAnimal(e.target.value);
+                }}
+                onBlur={(e) => {
+                  setAnimal(e.target.value);
+                }}
+                className="form-select"
+                aria-label="Animal select"
+              >
+                <option selected />
+                {ANIMALS.map((animal) => {
+                  return (
+                    <option value={animal} key={animal}>
+                      {animal}
+                    </option>
+                  );
+                })}
+              </select>
+            </label>
+            {""}
+            <label htmlFor="breed">
+              Breed
+              <select
+                id="breed"
+                value={breed}
+                onChange={(e) => setBreed(e.target.value)}
+                onBlur={(e) => setBreed(e.target.value)}
+                className="form-select w-auto"
+                aria-label="Breed select"
+              >
+                <option selected />
+                {breedOptions.map((breed) => (
+                  <option value={breed} key={breed}>
+                    {breed}
+                  </option>
+                ))}
+              </select>
+            </label>
+            {""}
+            <button type="submit" className="btn btn-outline-warning">
+              Submit
+            </button>
+          </form>
+        </Col>
+        <Col xs={6} md={4}>
+          {/* <NavLink to={"/favorite"}>Favorites</NavLink> */}
+          <LinkContainer to="/favorite">
+            <Nav.Link>
+              <Button variant="success">Your Saved Pets</Button>
+            </Nav.Link>
+          </LinkContainer>
+        </Col>
+      </Row>
+
       <FavoritesContext.Consumer>
         {(value) => (
           <PetList
@@ -94,6 +136,6 @@ export default function HomePage() {
           />
         )}
       </FavoritesContext.Consumer>
-    </div>
+    </Container>
   );
 }
