e3d8d0d5cc076c286141e67dc549fbf4e13287e2
Belinda Shan <belindashan126@hotmail.com>
2023-04-16 14:07:26 +1000

    finished the FavoritesPage

--- src/components/FavoritesPage/index.js
+++ src/components/FavoritesPage/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { FavoritesContext } from "../App";
 
 export default function FavoritesPage() {
@@ -6,7 +7,18 @@ export default function FavoritesPage() {
     <FavoritesContext.Consumer>
       {(value) =>
         value.favorites.map((fav) => {
-          return <div>Favorites {fav.name}</div>;
+          return (
+            <>
+              <Link to={`/detail/${fav.id}`}>
+                <img src={fav.images[0]} alt={fav.name} />
+              </Link>
+              <div>
+                <h2>{fav.name}</h2>
+                <h3>{fav.breed}</h3>
+                <h3>{fav.animal}</h3>
+              </div>
+            </>
+          );
         })
       }
     </FavoritesContext.Consumer>
